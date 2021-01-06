5df56feffcd00182aa8942e80f8d09f1e2e749e9
susilon <54101273+susilon@users.noreply.github.com>
2021-01-06 15:00:51 +0700

    Add Signer and Verification Sample

--- bin/clitest.dart
+++ bin/clitest.dart
@@ -6,14 +6,26 @@ void main() async {
   final publicKey = await parseKeyFromFile<RSAPublicKey>('files/rsa.public');
   final privKey = await parseKeyFromFile<RSAPrivateKey>('files/rsa.private');
 
+// encrypt decrypt
+  print('encrypt decrypt');
   final plainText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
-  final encrypter = Encrypter(RSA(publicKey: publicKey, privateKey: privKey));
-
+  final encrypter = Encrypter(RSA(publicKey: publicKey));
   final encrypted = encrypter.encrypt(plainText);
-  final decrypted = encrypter.decrypt(encrypted);
-
-  print(decrypted);
-  print(encrypted.bytes);
-  print(encrypted.base16);
+  //print(encrypted.bytes);
+  //print(encrypted.base16);
   print(encrypted.base64);
+  // --
+  final decrypter = Encrypter(RSA(privateKey: privKey));
+  final decrypted = decrypter.decrypt(encrypted);
+  print(decrypted);
+
+// signature verification
+  print('signature verification');
+  final signer = Signer(RSASigner(RSASignDigest.SHA256, privateKey: privKey));
+  final signedtext = signer.sign(decrypted).base64;
+  print(signedtext);
+  // --
+  final verifier =
+      Signer(RSASigner(RSASignDigest.SHA256, publicKey: publicKey));
+  print(verifier.verify64(plainText, signedtext));
 }
